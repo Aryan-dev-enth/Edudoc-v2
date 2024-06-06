b1eb6f2f7fcc4d7dbb21afa001f37bbd42982e43
Aryan Singh <aryan28november@gmail.com>
2024-06-07 00:24:29 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -3,22 +3,33 @@ import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
 import connectDb from './config/connect.js';
-import notesRoutes from './routes/notesRoutes.js'
-
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const db_url = process.env.MONGODB_URI;
 const db_name = process.env.MONGODB_NAME;
 
-app.use(cors()); 
+// Set up CORS middleware
+const corsOptions = {
+  origin: '*', // Adjust this to restrict access to specific origins if needed
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
 
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use("/api/notes", notesRoutes );
+// API routes
+app.use("/api/notes", notesRoutes);
 
+// Connect to the database
 connectDb(db_url, db_name);
 
+// Start the server
 app.listen(port, () => {
-    console.log("Server running at ", port);
+  console.log(`Server running at http://localhost:${port}`);
 });
