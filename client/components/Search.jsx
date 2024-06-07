900d8ca5f3ebc6e8f72414352ecd499ff59fffc3
Aryan Singh <aryan28november@gmail.com>
2024-06-07 20:57:37 +0530

    improved constants

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({ searchQuery, setSearchQuery }) => {
   return (
     <input
       type="text"
-      placeholder="Search for notes, pyw, and subjects..."
+      placeholder="Search for Notes, PYQ, and Subjects..."
       className="w-11/12 max-w-3xl px-6 py-3 md:px-8 md:py-4 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-base md:text-lg"
       value={searchQuery}
       onChange={e => setSearchQuery(e.target.value)}
