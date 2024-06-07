9ab9b075f444bbd1d67eadf17d22f952e316b515
Aryan Singh <aryan28november@gmail.com>
2024-06-07 13:43:44 +0530

    fixed fileCard

--- client/components/data/FileCard.jsx
+++ client/components/data/FileCard.jsx
@@ -1,8 +1,9 @@
+
 import React from "react";
 
 const FileCard = (props) => {
   const { name, description, imageUrl, iconUrl } = props.data;
-
+  
   
   const truncateText = (text, maxLength) => {
     if (text.split(" ").length > 1 && text.length > maxLength) {
@@ -12,14 +13,14 @@ const FileCard = (props) => {
   };
 
   return (
-    <div className="w-full m-2 sm:w-[250px] h-[100px] rounded-lg px-4 py-2 shadow-lg hover:bg-blue-100 flex justify-between items-center cursor-pointer">
+    <div className="w-full m-2 lg:w-[250px] h-[100px] rounded-lg px-4 py-2 shadow-lg hover:bg-blue-100 flex justify-between items-center cursor-pointer">
       <div className="left w-[60px] h-[60px] flex justify-center items-center">
-        <a href={imageUrl} target="_blank"><img src="edudoc.jpg" alt="" className="w-full h-full rounded-lg" /></a>
+        <img src="edudoc.jpg" alt="" className="w-full h-full rounded-lg" />
       </div>
       <div className="right flex flex-col justify-center flex-grow ml-4">
-        <h1 className="text-blue-400 font-semibold overflow-hidden overflow-ellipsis whitespace-nowrap">
+        <a href={imageUrl}><h1 className="text-blue-400 font-semibold overflow-hidden overflow-ellipsis whitespace-nowrap">
           {truncateText(name, 10)}
-        </h1>
+        </h1></a>
         <div className="flex items-center gap-2">
           <img src={iconUrl} alt="" className="w-[20px] h-[20px]" />
           <h4 className="text-xs overflow-hidden overflow-ellipsis whitespace-nowrap">
