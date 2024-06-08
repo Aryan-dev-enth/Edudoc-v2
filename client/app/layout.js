4262d6e8f8a24ffdbf287fb33839e0b3595d40d8
Aryan Singh <aryan28november@gmail.com>
2024-06-08 11:21:13 +0530

    cursor

--- client/app/layout.js
+++ client/app/layout.js
@@ -5,7 +5,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import AnimatedCursor from "react-animated-cursor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,13 +23,7 @@ export default function RootLayout({ children }) {
           {children}
           <Analytics />
           <SpeedInsights />
-          <AnimatedCursor color="193, 193, 0"
-            innerScale={0.5}
-            outerScale={3}
-            outerSize={15}
-            outerAlpha={0.4}
-            
-          />
+          
         </body>
       </html>
     </ClerkProvider>
